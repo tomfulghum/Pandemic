993b65bdc872caf64e9503032c73bde10e4b1fb5
Philipp <phglas@stud.mediadesign.de>
2019-07-08 18:22:51 +0200

    Moving hook points bug fixes

--- Assets/_Game/Scripts/ObjectFloating.cs
+++ Assets/_Game/Scripts/ObjectFloating.cs
@@ -6,16 +6,17 @@ public class ObjectFloating : MonoBehaviour
 {
     public float speed;
     public float height;
+
+    Vector2 startPosition;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 pos = transform.position;
-        transform.position = new Vector2(pos.x, pos.y + Mathf.Sin(Time.time * speed) * height);
+        transform.position = startPosition + new Vector2(0 , Mathf.Sin(Time.fixedTime  * speed) * height);
     }
 }
