07d86b9bccc9e842c86f0ba49ddd9d41cb592651
Alex <alexsunbird95@gmail.com>
2019-07-11 14:30:14 +0200

    added main menu setup

---
+++ Assets/_Game/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        //TODO
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+}
---
+++ Assets/_Game/Scripts/Menus/RollingCredits.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollingCredits : MonoBehaviour
+{
+    [SerializeField] int ScrollSpeedPerSecond;
+    [SerializeField] int Delay;
+    RectTransform Credits;
+    Vector3 maxVector;
+
+    private void Awake()
+    {
+        Credits = GetComponent<RectTransform>();
+    }
+
+    private void Start()
+    {
+        maxVector = new Vector3(0, Delay, 0);
+    }
+
+    void OnEnable()
+    {
+        Credits.anchoredPosition3D = new Vector3(0, -2900, 0);
+    }
+
+    void Update()
+    {
+        Credits.anchoredPosition3D += new Vector3(0, ScrollSpeedPerSecond * Time.deltaTime, 0);
+
+        if (Credits.anchoredPosition3D.y > maxVector.y)
+        {
+            Credits.anchoredPosition3D = new Vector3(0, -2530, 0);
+        }
+    }
+}
