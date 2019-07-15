2f86a164aeb9fb83d3b6d8c82a12452aa76699ee
Philipp <phglas@stud.mediadesign.de>
2019-07-15 13:42:51 +0200

    finalized first version of throw

--- Assets/_Game/Scripts/Player/PlayerHook.cs
+++ Assets/_Game/Scripts/Player/PlayerHook.cs
@@ -34,6 +34,7 @@ using UnityEngine;
 //hook state --> nur checken wenn hook nicht active ist
 //pull object to player --> object stays with player --> throw object
 //vllt während das object auf den spieler zufliegt layer collission ausmachen damit es nirgendwo hängen bleiben kann
+//evtl passen visuals bei dem wurf nicht zusammen weil die visuals andere werte haben als der wurf
 public class PlayerHook : MonoBehaviour
 {
     //enum HookState {Targeting, Hooking, usw...} 
@@ -83,7 +84,8 @@ public class PlayerHook : MonoBehaviour
     bool PullBackActive;
     bool PullToBigEnemy;
     bool HookCancelled;
-    bool PullTargetToPlayer;
+    [HideInInspector] public bool PullTargetToPlayer;
+    bool AimingObject;
 
     float timeslowTest;
     float currentTimeActive;
@@ -95,6 +97,7 @@ public class PlayerHook : MonoBehaviour
     Vector2 TargetPoint;
     Vector2 TargetPosition;
     Vector2 ControllerDirection;
+    Vector2 ContDirWithoutDeadzone;
 
     Coroutine HookCooldown = null;
 
@@ -111,6 +114,8 @@ public class PlayerHook : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       // Debug.Log(TargetToPull);
+        //Debug.Log(CurrentSelectedPoint);
         if (Input.GetAxis("Horizontal") < -ControllerTolerance || Input.GetAxis("Horizontal") > ControllerTolerance || Input.GetAxis("Vertical") < -ControllerTolerance || Input.GetAxis("Vertical") > ControllerTolerance)
         {
             if (stick == ControllStick.Left)
@@ -126,6 +131,11 @@ public class PlayerHook : MonoBehaviour
                 ControllerDirection = ControllerDirection.normalized;
             }
         }
+        if(Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            ContDirWithoutDeadzone.x = Input.GetAxis("Horizontal");
+            ContDirWithoutDeadzone.y = Input.GetAxis("Vertical");
+        }
         //Debug.Log(Input.GetAxis("ControllerHook"));
         if ((HookActive == false || CanUseHook()) && PullBackActive == false && GetComponent<PlayerCombat>().Smashing == false && PullTargetToPlayer == false) //vllt hook erst auf nicht active wetzen wenn ziel erreicht ist &&hookNotThrown
         {
@@ -135,22 +145,49 @@ public class PlayerHook : MonoBehaviour
                 if (TargetToPull == null)
                 {
                     StartAiming();
-                } else
+                }
+                else
                 {
-                    if(Input.GetButtonDown("Fire1"))
+                    if (Input.GetButtonDown("Fire1"))
                     {
+                        //Debug.Log("object drop");
+                        TargetToPull.GetComponent<ThrowableObject>().PickedUp = false;
+                        TargetToPull.GetComponent<ThrowableObject>().ObjectToFollow = null;
                         TargetToPull = null;
+                        CurrentSelectedPoint = null;
                         GetComponent<PlayerMovement>().DisableUserInput(false);
                         GetComponent<VisualizeTrajectory>().RemoveVisualeDots();
                     }
-                    AimThrow();
+                    else
+                    {
+                        AimThrow(ContDirWithoutDeadzone);
+                    }
+                    HookActivated = true;
                     //AimThrowableObject --> else if (Aim == ture) { GetComponent<ThrowableObject>().Launch() } //braucht noch eine variable --> currently thrown --> behandelt dann selbst alle physik berechnungen
                 }
             }
             else if ((Input.GetButtonUp("Hook") || Input.GetAxis("ControllerHook") == 0) && HookActivated == true) //für controller ist das blöd //evtl nicht == 0 sondern ungleich 1
             {
                 CurrentlyAiming = false;
+                if (AimingObject && TargetToPull != null)
+                {
+                    GetComponent<VisualizeTrajectory>().RemoveVisualeDots();
+                   // float xDir = Input.GetAxis("Horizontal"); //kein null check biser
+                    //float yDir = Input.GetAxis("Vertical"); //--> evtl überprüfen ob wir eine deadzone wollen
+                    
+                    float strength = Mathf.Lerp(MinThrowVelocity, MaxThrowVelocity, (Mathf.Abs(ContDirWithoutDeadzone.x) + Mathf.Abs(ContDirWithoutDeadzone.y)));
+                    //Debug.Log(TargetToPull);
+                    Vector2 velocity = new Vector2(-ContDirWithoutDeadzone.x, -ContDirWithoutDeadzone.y).normalized * strength;
+                    TargetToPull.GetComponent<ThrowableObject>().Throw(velocity);
+                    TargetToPull.GetComponent<ThrowableObject>().PickedUp = false;
+                    TargetToPull.GetComponent<ThrowableObject>().ObjectToFollow = null;
+                    TargetToPull = null;
+                    CurrentSelectedPoint = null;
+                    TargetHookPoint = null;
+                    GetComponent<PlayerMovement>().DisableUserInput(false);
+                }
                 ActivateHook(ControllerDirection);
+                AimingObject = false;
             }
         }
 
@@ -158,11 +195,12 @@ public class PlayerHook : MonoBehaviour
         {
             HookToTarget();
         }
-        if(PullTargetToPlayer)
+        if (PullTargetToPlayer)
         {
             if (Vector2.Distance(transform.position, TargetToPull.transform.position) < TargetReachedTolerance)
             {
                 DeactivatePullToTarget();
+                //TargetToPull.GetComponent<ThrowableObject>().ObjectToFollow = transform;
             }
             else
             {
@@ -172,10 +210,15 @@ public class PlayerHook : MonoBehaviour
         }
     }
 
-    void AimThrow() //evlt während aim den spieler anhalten oder bewegung verlangsamen
-    {
+    void AimThrow(Vector2 _direction) //evlt während aim den spieler anhalten oder bewegung verlangsamen //schauen ob man die deadzone lassen kann ansonsten als parameter übergeben
+    { //evlt die velocity zurückgeben
+        AimingObject = true;
         GetComponent<PlayerMovement>().DisableUserInput(true);
-        Vector2 throwVelocity = new Vector2(-ControllerDirection.x, -ControllerDirection.y).normalized * HookSpeed;
+       // float xDir = Input.GetAxis("Horizontal");
+        //float yDir = Input.GetAxis("Vertical"); //--> evtl überprüfen ob wir eine deadzone wollen
+        //float velocity = Mathf.Lerp(MinThrowVelocity, MaxThrowVelocity, (Mathf.Abs(xDir) + Mathf.Abs(yDir)));
+        float velocity = Mathf.Lerp(MinThrowVelocity, MaxThrowVelocity, (Mathf.Abs(ContDirWithoutDeadzone.x) + Mathf.Abs(ContDirWithoutDeadzone.y)));
+        Vector2 throwVelocity = new Vector2(-ContDirWithoutDeadzone.x, -ContDirWithoutDeadzone.y).normalized * velocity; //HookSpeed --> "ThrowForce" --> wenn wir den velocity ansatz nicht verwenden dann das
         GetComponent<VisualizeTrajectory>().VisualizeDots(transform.position, throwVelocity, Gravity);
     }
 
@@ -190,7 +233,7 @@ public class PlayerHook : MonoBehaviour
         }
         Time.timeScale = NormalTimeScale;
         Time.fixedDeltaTime = Time.timeScale * 0.02f;
-       
+
         if (CurrentSelectedPoint != null && CurrentSelectedPoint.CompareTag("HookPoint"))
         {
             AdditionalTravelTest = true;
@@ -217,14 +260,22 @@ public class PlayerHook : MonoBehaviour
             */
             currentTimeActive = 0; //hier evtl falsch
         }
-        else if(CurrentSelectedPoint != null && CurrentSelectedPoint.CompareTag("Throwable"))
+        else if (CurrentSelectedPoint != null && CurrentSelectedPoint.CompareTag("Throwable") && CurrentSelectedPoint.GetComponent<ThrowableObject>().CurrentlyThrown == false) 
         {
             PullTargetToPlayer = true;
             TargetToPull = CurrentSelectedPoint.gameObject;
             GetComponent<PlayerMovement>().DisableUserInput(true);
+            TargetToPull.GetComponent<ThrowableObject>().PickedUp = true;
+            TargetToPull.GetComponent<ThrowableObject>().ObjectToFollow = transform;
             currentTimeActive = 0;
         }
-        else if (TargetHookPoint == null)
+        else if(CurrentSelectedPoint != null && CurrentSelectedPoint.CompareTag("RopePoint"))
+        {
+            Debug.Log("start rope fight");
+            GetComponent<PlayerMovement>().DisableUserInput(true);
+            //Debug.DrawLine(transform.position, CurrentSelectedPoint.transform.position);
+        }
+        else if (TargetHookPoint == null && AimingObject == false)
         {
             if (controlls == ControllType.Keyboard)
             {
@@ -342,7 +393,7 @@ public class PlayerHook : MonoBehaviour
     {
         if (Time.timeScale > MaxTimeSlow)
         {
-            Time.timeScale -= Mathf.Pow(x,2);
+            Time.timeScale -= Mathf.Pow(x, 2);
             Time.fixedDeltaTime = Time.timeScale * 0.02f;
         }
     }
@@ -450,7 +501,7 @@ public class PlayerHook : MonoBehaviour
     IEnumerator JumpBack()
     {
         int x = 1;
-        if(transform.position.x > TargetHookPoint.transform.parent.transform.position.x)
+        if (transform.position.x > TargetHookPoint.transform.parent.transform.position.x)
         {
             x = 1;
         }
@@ -458,7 +509,7 @@ public class PlayerHook : MonoBehaviour
         {
             x = -1;
         }
-        Vector2 JumpBackvelocity = new Vector2(0.5f*x, 0.5f).normalized * HookSpeed; //evtl jump speed
+        Vector2 JumpBackvelocity = new Vector2(0.5f * x, 0.5f).normalized * HookSpeed; //evtl jump speed
         GetComponent<PlayerMovement>().SetExternalVelocity(JumpBackvelocity);
         yield return new WaitForSeconds(0.4f); //bessere lösung finden
         GetComponent<PlayerMovement>().DisableUserInput(false); //wahrscheinlich irgendwo einmal zu oft aufgerufen
@@ -474,7 +525,7 @@ public class PlayerHook : MonoBehaviour
 
     Collider2D FindTargetHookPoint(Vector2 _direction) //evtl besser als find target oder so --> noch überlegn wie man die vector2.zero geschichte besser lösen könnte
     {
-        if(_direction == Vector2.zero)
+        if (_direction == Vector2.zero)
         {
             return null;
         }
@@ -494,13 +545,13 @@ public class PlayerHook : MonoBehaviour
         for (int i = 0; i < ColliderInRange.Length; i++)
         {
             RaycastHit2D hit = Physics2D.Raycast(transform.position, (ColliderInRange[i].transform.position - transform.position), HookRadius, layer_mask);
-            if (hit.collider != null && (hit.collider.CompareTag("HookPoint") || hit.collider.CompareTag("Throwable")))
+            if (hit.collider != null && (hit.collider.CompareTag("HookPoint") || hit.collider.CompareTag("Throwable") || hit.collider.CompareTag("RopePoint")))
             {
                 VisualizeLines(transform.position, hit.collider.transform.position);
                 hit.collider.GetComponent<SpriteRenderer>().color = Color.red;
                 Vector2 PlayerToCollider = (ColliderInRange[i].transform.position - transform.position).normalized;
                 //Vector2 PlayerToMouse = (MousePosition - transform.position).normalized;
-                if (hit.collider.CompareTag("HookPoint"))
+                if (hit.collider.CompareTag("HookPoint") || hit.collider.CompareTag("RopePoint"))
                 {
                     float angleInDeg = Vector2.Angle(PlayerToCollider, _direction);
                     if (angleInDeg < Angle && angleInDeg < LowestAngle)
@@ -531,7 +582,7 @@ public class PlayerHook : MonoBehaviour
     {
         for (int i = 0; i < TotalHookPoints.Count; i++)
         {
-            if (TotalHookPoints[i].CompareTag("HookPoint") || TotalHookPoints[i].CompareTag("Throwable"))
+            if (TotalHookPoints[i].CompareTag("HookPoint") || TotalHookPoints[i].CompareTag("Throwable") || TotalHookPoints[i].CompareTag("RopePoint"))
             {
                 TotalHookPoints[i].GetComponent<SpriteRenderer>().color = Color.white;
             }
--- Assets/_Game/Scripts/VisualizeTrajectory.cs
+++ Assets/_Game/Scripts/VisualizeTrajectory.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class VisualizeTrajectory : MonoBehaviour
 {
     [Range(0,100)] public int NumOfVisualDots;
+    public float TimeBetweenDots; //besseren namen finden
     public GameObject DotPrefab;
+    public LayerMask layer_mask;
     GameObject DotParent;
+    //num of visual dots ersetzen durch dichte der punkte --> oder zeitabstand?
     // Start is called before the first frame update
     void Start()
     {
@@ -22,9 +25,29 @@ public class VisualizeTrajectory : MonoBehaviour
     public void VisualizeDots(Vector2 _startPosition, Vector2 _launchVelocity ,float _gravity) //gravity sollte irgendwoanders gesetzt werden --> am besten von dem object das geworfen wird
     {
         RemoveVisualeDots();
+        DotParent.transform.position = _startPosition;
         //float TravelTime = CalculateTravelTime( _startPosition,  _launchVelocity, new Vector2(0, -_gravity));
         //float TravelTimeForEachDot = TravelTime / NumOfVisualDots;
-        DotParent.transform.position = _startPosition;
+        bool HitSmth = false;
+        float ThrowTime = 0f;
+        while(HitSmth == false && DotParent.transform.childCount < NumOfVisualDots) //|| currentnum of dots > max num of visualdots --> als sicherung --> da funktioniert noch was nicht sogut
+        {
+            Vector2 StartPosition = CalculatePosition(ThrowTime, _launchVelocity, DotParent.transform.position, new Vector2(0, -_gravity));
+            ThrowTime += TimeBetweenDots; //dafür variable aus dem editor nehmen 
+            Vector2 TargetPosition = CalculatePosition(ThrowTime, _launchVelocity, DotParent.transform.position, new Vector2(0, -_gravity));
+            float RaycastLenght = (TargetPosition - StartPosition).magnitude;
+            RaycastHit2D hit = Physics2D.Raycast(StartPosition, (TargetPosition - StartPosition), RaycastLenght, layer_mask); //vllt anstatt 1 irgendwas ausrechnen?
+            if(hit.collider == null)
+            {
+                GameObject trajectoryDot = Instantiate(DotPrefab);
+                trajectoryDot.transform.SetParent(DotParent.transform);
+                trajectoryDot.transform.position = StartPosition;
+            } else
+            {
+                HitSmth = true;
+            }
+        }
+        /*
         for (int i = 0; i < NumOfVisualDots; i++)
         {
             GameObject trajectoryDot = Instantiate(DotPrefab);
@@ -32,6 +55,7 @@ public class VisualizeTrajectory : MonoBehaviour
             trajectoryDot.transform.position = CalculatePosition(0.05f * i, _launchVelocity, DotParent.transform.position, new Vector2(0, -_gravity));
 
         }
+        */
     }
 
     public void RemoveVisualeDots()
