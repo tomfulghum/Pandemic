62056e4de14698b2a9b3ed2dba39ff3d7b61c9d2
Alex <alexsunbird95@gmail.com>
2019-07-16 15:00:40 +0200

    5 basic test particle systems

---
+++ Assets/MotionTrail.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotionTrail : MonoBehaviour
+{
+    [SerializeField] private Actor2D actor;
+
+    void Start()
+    {
+        actor = GetComponent<Actor2D>();
+    }
+    
+    void Update()
+    {
+        Vector2 vel = actor.velocity;
+    }
+}
