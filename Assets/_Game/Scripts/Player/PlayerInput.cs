962778a1173e8649cb6052511a36c309687ea577
Frederic Fulghum <mail@tomfulghum.net>
2019-07-16 10:49:01 +0200

    Put input bindings into a scriptable object

--- Assets/_Game/Scripts/Player/PlayerInput.cs
+++ Assets/_Game/Scripts/Player/PlayerInput.cs
@@ -8,22 +8,18 @@ public class PlayerInput : MonoBehaviour
     //************************//
 
     [SerializeField] private int playerId = 0;
-    [SerializeField] private string jumpButtonName = "Jump";
-    [SerializeField] private string hookButtonName = "Hook";
-    [SerializeField] private string moveHorizontalAxisName = "MoveHorizontal";
-    [SerializeField] private string aimHorizontalAxisName = "AimHorizontal";
-    [SerializeField] private string aimVerticalAxisName = "AimVertical";
+    [SerializeField] private InputBindings bindings = default;
 
     //******************//
     //    Properties    //
     //******************//
 
     public Player player { get; private set; }
-    public string jumpButton { get { return jumpButtonName; } }
-    public string hookButton { get { return hookButtonName; } }
-    public string moveHorizontalAxis { get { return moveHorizontalAxisName; } }
-    public string aimHorizontalAxis { get { return aimHorizontalAxisName; } }
-    public string aimVerticalAxis { get { return aimVerticalAxisName; } }
+    public string jumpButton { get { return bindings.jumpButtonName; } }
+    public string hookButton { get { return bindings.hookButtonName; } }
+    public string moveHorizontalAxis { get { return bindings.moveHorizontalAxisName; } }
+    public string aimHorizontalAxis { get { return bindings.aimHorizontalAxisName; } }
+    public string aimVerticalAxis { get { return bindings.aimVerticalAxisName; } }
 
     //*******************************//
     //    MonoBehaviour Functions    //
