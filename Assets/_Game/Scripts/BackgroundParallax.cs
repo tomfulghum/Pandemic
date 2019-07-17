9b45b8d8c4b642e124989ed0a47cd35afd6077cd
Frederic Fulghum <mail@tomfulghum.net>
2019-07-17 15:14:53 +0200

    First Parallax test

---
+++ Assets/_Game/Scripts/BackgroundParallax.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Cinemachine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+
+public class BackgroundParallax : MonoBehaviour
+{
+    [SerializeField] private float parallaxFactor = 0f;
+
+    private float lastCameraPosition = 0;
+
+    private void Update()
+    {
+        CinemachineVirtualCamera activeCamera = (CinemachineVirtualCamera)Camera.main.GetComponent<CinemachineBrain>().ActiveVirtualCamera;
+
+        if (activeCamera) {
+            if (lastCameraPosition != 0) {
+                transform.position += Vector3.left * (lastCameraPosition - activeCamera.transform.position.x) * parallaxFactor;
+            }
+
+            lastCameraPosition = activeCamera.transform.position.x;
+        }
+    }
+}
