d1dba4aef5e0a6c870507e9793b35a9fd62793be
Philipp <phglas@stud.mediadesign.de>
2019-07-24 11:35:06 +0200

    Added HookPoint Animations

--- Assets/_Game/Scripts/Player/PlayerAnim.cs
+++ Assets/_Game/Scripts/Player/PlayerAnim.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerAnim : MonoBehaviour
 {
+    bool FacingLeft;
+    float xAxis;
+
     Actor2D actor;
     Animator anim;
     // Start is called before the first frame update
@@ -11,12 +14,13 @@ public class PlayerAnim : MonoBehaviour
     {
         actor = GetComponent<Actor2D>();
         anim = GetComponent<Animator>();
+        xAxis = Input.GetAxis("Horizontal");
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(actor.velocity.magnitude);
+        SetFacingDirection();
         if(Input.GetAxis("Horizontal") < -0.15f || Input.GetAxis("Horizontal") > 0.15f)
         {
             anim.SetFloat("RunSpeed", actor.velocity.magnitude);
@@ -24,7 +28,7 @@ public class PlayerAnim : MonoBehaviour
         {
             anim.SetFloat("RunSpeed", -0.1f);
         }
-        if(GetComponent<PlayerCombat>().FacingLeft == false)
+        if(FacingLeft == false)
         {
             GetComponent<SpriteRenderer>().flipX = false;
         } else
@@ -32,4 +36,18 @@ public class PlayerAnim : MonoBehaviour
             GetComponent<SpriteRenderer>().flipX = true;
         }
     }
+
+    void SetFacingDirection() //only for anim //can differ from facing direc in player combat //should change later
+    {
+        float CurrentJoystickDirection = Input.GetAxis("Horizontal");
+        if (CurrentJoystickDirection != xAxis)
+        {
+            if (CurrentJoystickDirection < 0)
+                FacingLeft = true;
+            else if (CurrentJoystickDirection > 0)
+                FacingLeft = false;
+            xAxis = CurrentJoystickDirection;
+        }
+    }
+
 }
