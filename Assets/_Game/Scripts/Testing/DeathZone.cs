3d3e66a18df7bf3bc4cb0fff106e083df472a361
Yannick <arndt.yannick@gmail.com>
2019-07-26 16:23:21 +0200

    new Level Style in the works

---
+++ Assets/_Game/Scripts/Testing/DeathZone.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour
+{
+    private GameObject myPlayer;
+    private Vector3 v3OldPos;
+    private int iFrames;
+
+    private void Start()
+    {
+        myPlayer = GameObject.Find("Player");
+        v3OldPos = myPlayer.transform.position;
+        iFrames = 0;
+    }
+
+    private void Update()
+    {
+        iFrames++;
+        if (iFrames >= 90)
+        {
+            if (!myPlayer.GetComponent<Actor2D>().collision.below)
+                return;
+            if (myPlayer.GetComponent<Actor2D>().collision.below.gameObject.layer == 8)
+            {
+                v3OldPos = myPlayer.transform.position;
+                iFrames = 0;
+                Debug.Log("Saved");
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.transform.CompareTag("Player"))
+            other.transform.position = v3OldPos;
+    }
+}
---
+++ Assets/_Game/Scripts/Testing/Keys.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Keys : MonoBehaviour
+{
+    [SerializeField]
+    private int m_iNormalKeys;
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("NormalKey") || other.CompareTag("BossKey"))
+        {
+            if (other.CompareTag("NormalKey"))
+                m_iNormalKeys++;
+            
+            Destroy(other.gameObject);
+        }
+
+        if (other.CompareTag("KeyDoor") && m_iNormalKeys >= 4)
+        {
+            m_iNormalKeys = -4;
+            Destroy(other.gameObject);
+        }
+    }
+}
