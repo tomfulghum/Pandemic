db548cbf0a94e4134004baa855b0f74114eba23c
Alex <alexsunbird95@gmail.com>
2019-08-08 17:35:12 +0200

    rework main menu

--- Assets/MenuButton.cs
+++ Assets/MenuButton.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using TMPro;
 public class MenuButton : MonoBehaviour
 {
@@ -9,38 +10,24 @@ public class MenuButton : MonoBehaviour
     //   Inspector Fields   //
     //**********************//
 
-    [SerializeField] int thisIndex;
-    [SerializeField] Color unselectedColor;
-    [SerializeField] Color selectedColor;
-
-    //*******************//
-    //   Public Fields   //
-    //*******************//
-
-    public TextMeshProUGUI textMeshPro;
+    [SerializeField] private Color unselectedColor;
+    [SerializeField] private Color selectedColor;
+    [SerializeField] private TextMeshProUGUI textMeshPro;
     
-    //********************//
-    //   Private Fields   //
-    //********************//
-
-    private ButtonIndex thisMenu;
-
     //*******************************//
     //    MonoBehaviour Functions    //
     //*******************************//
 
     private void Start()
     {
-        textMeshPro = GetComponent<TextMeshProUGUI>();
-        thisMenu = transform.parent.GetComponentInParent<ButtonIndex>();
     }
     void Update()
     {
-        if (thisMenu.index == thisIndex) {
+        if (gameObject == EventSystem.current.currentSelectedGameObject) {
             textMeshPro.color = selectedColor;
-            if (Input.GetButtonDown("Submit")) {
-                transform.parent.GetComponent<Button>().onClick.Invoke();
-            }
+            //if (Input.GetButtonDown("Submit")) {
+            //    GetComponent<Button>().onClick.Invoke();
+            //}
         } else {
             textMeshPro.color = unselectedColor;
         }
--- Assets/_Game/Scripts/Menus/MainMenu.cs
+++ Assets/_Game/Scripts/Menus/MainMenu.cs
@@ -1,13 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; 
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private Button m_backButton;
+
     //*******************************//
     //    MonoBehaviour Functions    //
     //*******************************//
 
+    public void SetBackButton(Button _button)
+    {
+        m_backButton = _button;
+    }
+    private void Update()
+    {
+        if (Input.GetButtonDown("Cancel") && m_backButton != null) {
+            m_backButton.onClick.Invoke();
+        }
+    }
     public void PlayGame()
     {
         GameManager.Instance.LoadGame();
