cddc9e9604b2e86b9f887a3622705b40355bf580
Philipp <phglas@stud.mediadesign.de>
2019-08-09 11:47:52 +0200

    new Player anim / Idle, run, dash, hit

--- Assets/_Game/Scripts/Player/PlayerAnim.cs
+++ Assets/_Game/Scripts/Player/PlayerAnim.cs
@@ -32,30 +32,29 @@ public class PlayerAnim : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        SetFacingDirection();
         if (Input.GetAxis("Horizontal") < -0.15f || Input.GetAxis("Horizontal") > 0.15f)
-        {
-            m_anim.SetFloat("RunSpeed", m_rb.velocity.magnitude);
-        }
+            m_anim.SetBool("Moving", true);
         else
-        {
-            m_anim.SetFloat("RunSpeed", -0.1f);
-        }
-        if(m_pc.currentAttackState == PlayerCombat.AttackState.Dash)
-        {
-            m_anim.SetBool("Dash", true);
-        } else
-        {
-            m_anim.SetBool("Dash", false);
-        }
+            m_anim.SetBool("Moving", false);
 
-        UpdateCollider(GetComponent<SpriteRenderer>().flipX);
-        if (m_pc.currentAttackState != PlayerCombat.AttackState.Dash)
+        if (PlayerHook.CurrentPlayerState == PlayerHook.PlayerState.Disabled)
+            m_anim.SetBool("Hit", true);
+        else
+            m_anim.SetBool("Hit", false);
+
+        if (m_pc.currentAttackState == PlayerCombat.AttackState.Dash)
+            m_anim.SetBool("Dash", true);
+        else
+            m_anim.SetBool("Dash", false);
+
+        UpdateCollider(m_facingLeft); //evtl auch ! facing left
+        if (m_pc.currentAttackState != PlayerCombat.AttackState.Dash && PlayerHook.CurrentPlayerState != PlayerHook.PlayerState.Disabled)
         {
-            if (m_facingLeft == false)
-                GetComponent<SpriteRenderer>().flipX = false;
+            SetFacingDirection();
+            if (m_facingLeft)
+                m_anim.SetFloat("FacingLeft", 1f);
             else
-                GetComponent<SpriteRenderer>().flipX = true;
+                m_anim.SetFloat("FacingLeft", 0f);
         }
     }
 
@@ -66,7 +65,7 @@ public class PlayerAnim : MonoBehaviour
     //*************************//
 
 
-    private void SetFacingDirection() //only for anim //can differ from facing direc in player combat //should change later
+    private void SetFacingDirection() //only for anim //can differ from facing direc in player combat //should change later 
     {
         float CurrentJoystickDirection = Input.GetAxis("Horizontal");
         if (CurrentJoystickDirection < 0)
